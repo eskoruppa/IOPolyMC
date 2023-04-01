17407fe75b0b0bd0e821c602971826f37bf7d0f2
esasen <esdrifter@gmail.com>
2023-04-01 18:52:49 +0200

    added functionality to generate PolyMC configurations by interpolating between points. This can be used to generate knotted initial configurations.

--- iopolymc/__init__.py
+++ iopolymc/__init__.py
@@ -9,20 +9,30 @@ Provides methods to read PolyMC output and write PolyMC input files
 # ~ __all__ = ["idb","restart","state","thetas","xyz"]
 
 from .idb       import readidb
+
 from .restart   import read_restart
 from .restart   import write_restart
+
 from .state     import load_state
 from .state     import read_spec
 from .state     import read_state
+
 from .thetas    import load_thetas
 from .thetas    import read_thetas
+
 from .xyz       import load_xyz
 from .xyz       import read_xyz
 from .xyz       import read_xyz_atomtypes
 from .xyz       import write_xyz
+
 from .genpdb    import state2pdb
 from .genpdb    import gen_pdb
+
 from .input     import read_input
 from .input     import write_input
 from .input     import querysims
 from .input     import simfiles
+
+from .pts2config import pts2config, config2triads, pts2xyz, pts2restart
+
+
---
+++ test_pts2config.py
@@ -0,0 +1,50 @@
+import numpy as np
+import iopolymc as iopmc
+
+"""
+    Generate a trefoil knot
+"""
+
+if __name__ == "__main__":
+
+    disc_len = 3.4
+    numbp   = 500
+
+    # pts = list()
+    # pts.append(np.array([0,0,0]))
+    # pts.append(np.array([0,0,40]))
+    # pts.append(np.array([0,10,40]))
+    # pts.append(np.array([-5,10,30]))
+    # pts.append(np.array([-5,-5,30]))
+    # pts.append(np.array([5,-5,35]))
+    # pts.append(np.array([5,5,35]))
+    # pts.append(np.array([-5,5,35]))
+    # pts.append(np.array([-5,5,45]))
+    # pts.append(np.array([0,0,45]))
+    # pts.append(np.array([0,0,80]))
+
+    pts = list()
+    pts.append(np.array([0,0,0]))
+    pts.append(np.array([0,0,80]))
+    pts.append(np.array([0,40,80]))
+    pts.append(np.array([-20,40,40]))
+    pts.append(np.array([-20,-20,40]))
+    pts.append(np.array([20,-20,60]))
+    pts.append(np.array([20,20,60]))
+    pts.append(np.array([-20,20,60]))
+    pts.append(np.array([-20,20,100]))
+    pts.append(np.array([0,0,100]))
+    pts.append(np.array([0,0,140]))
+
+    pts = np.array(pts)
+    pts *= 6
+    # pts.append(np.array([2,2,82]))
+
+    # config = pts2config(pts,disc_len,numbp=500)
+
+    xyzfn       = 'test/trefoil_large.xyz'
+    restartfn   = 'test/trefoil_large.restart'
+
+    numbp = 800
+    iopmc.pts2xyz(xyzfn,pts,disc_len,numbp=numbp)
+    iopmc.pts2restart(restartfn,pts,disc_len,numbp=numbp)
